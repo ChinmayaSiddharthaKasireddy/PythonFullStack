80a0072cfa5a96530aa09344f5c42c79540afab4
Siddharthakasireddy <siddharthakasireddy8@gmail.com>
2026-09-06 20:27:29 +0530

    Strings

--- Python/Day07.py
+++ Python/Day07.py
@@ -3,31 +3,35 @@
 str1 = "Hello"
 str2 = "World"
 result = str1 + " " + str2
-print(result) # Output: Hello World
+print(result)
 # Repetition
-print("Python! " * 3) # Output: Python! Python! Python!
+print("Python! " * 3)
 # Indexing
 text = "Python"
-print(text[0]) # Output: P (1st character)
-print(text[-1]) # Output: n (last character)
+print(text[0])
+print(text[-1])
 # Slicing
-print(text[0:3]) # Output: Pyt (from index 0 to 2)
-print(text[:4]) # Output: Pyth (default start is 0)
-print(text[2:]) # Output: thon (from index 2 to end)
+print(text[0:3])
+print(text[:4])
+print(text[2:])
 # Membership
-print('Pyt' in text) # Output: True
-print('Java' not in text) # Output: True
+print('Pyt' in text)
+print('Java' not in text)
 
 #Builtin String Functions
 
 # 1. len()
 text = "Hello World"
-print(len(text)) # Output: 11
+print(len(text))
 # 2. max() and min()
-print(max("abcXYZ")) # Output: 'c' (highest ASCII value)
-print(min("abcXYZ")) # Output: 'X' (lowest ASCII value)
+print(max("abcXYZ"))
+print(min("abcXYZ"))
 # 3. sorted()
-print(sorted("python")) # Output: ['h', 'n', 'o', 'p', 't', 'y'] (sorted characters in ascending order)
+print(sorted("python"))
 # 4. chr() and ord()
-print(ord('A')) # Output: 65 (ASCII value of 'A')
-print(chr(97)) # Output: 'a' (character for ASCII value 97)
+print(ord('A'))
+print(chr(97))
+print(ord('a'))
+print(chr(66))
+
+
